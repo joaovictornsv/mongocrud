82166e9b90fb132633147d08882c7806484365fd
Joao Victor Negreiros <joaovictornsv@gmail.com>
2021-05-31 12:32:11 -0300

    fix: lint fix

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,18 @@
-import 'reflect-metadata'
-import express from 'express'
+import 'reflect-metadata';
+import express from 'express';
+
+import mongoose from 'mongoose';
 
 const app = express();
 
-import mongoose from 'mongoose'
-
-mongoose.connect('mongodb://localhost/mongocrud', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect('mongodb://localhost/mongocrud', { useNewUrlParser: true, useUnifiedTopology: true });
 
 export const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
+db.once('open', () => {
   console.log('Connected!');
-  
 });
 
-app.get('/api', (req, res) => {
-  return res.status(200).json({message: "hello"})
-})
+app.get('/api', (req, res) => res.status(200).json({ message: 'hello' }));
 
-export { app }
+export { app };
