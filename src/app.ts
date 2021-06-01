313e1707f62fa17b1cc8885d2c8d124e0ed69b73
Joao Victor Negreiros <joaovictornsv@gmail.com>
2021-06-01 16:03:44 -0300

    feat: create post route

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import 'reflect-metadata';
 import express from 'express';
-
 import mongoose from 'mongoose';
+import { v4 as uuid } from 'uuid';
+import Profile from './entities/Profile';
 
 const app = express();
 
+app.use(express.json());
 mongoose.connect('mongodb://localhost/mongocrud', { useNewUrlParser: true, useUnifiedTopology: true });
 
 export const db = mongoose.connection;
@@ -13,6 +15,31 @@ db.once('open', () => {
   console.log('Connected!');
 });
 
-app.get('/api', (req, res) => res.status(200).json({ message: 'hello' }));
+app.get('/profiles', async (req, res) => {
+  const profiles = await Profile.find();
+
+  res.status(200).json(profiles);
+});
+
+app.post('/profiles', async (req, res) => {
+  const {
+    firstName,
+    lastName,
+    age,
+    position,
+    team,
+  } = req.body;
+
+  const profile = await Profile.create({
+    _id: uuid(),
+    firstName,
+    lastName,
+    age,
+    position,
+    team,
+  });
+
+  res.status(201).json(profile);
+});
 
 export { app };
