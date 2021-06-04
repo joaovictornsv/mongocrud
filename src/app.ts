73770e7ed4c1fc55ce899b8120e654ebb9a45a3a
Joao Victor Negreiros <joaovictornsv@gmail.com>
2021-06-04 13:26:20 -0300

    feat: add route post (teams)

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import 'reflect-metadata';
 import express from 'express';
 import Player from './entities/Player';
 import connectToDatabase from './database';
+import Team from './entities/Team';
 
 const app = express();
 
@@ -24,15 +25,41 @@ app.post('/players', async (req, res) => {
     team,
   } = req.body;
 
+  if (!team) {
+    return res.status(400).json({ message: 'Team not provided' });
+  }
+
+  const teamExists = await Team.findOne({ name: team });
+
+  if (!teamExists) {
+    return res.status(400).json({ message: 'Team provided not found' });
+  }
+
   const profile = await Player.create({
     firstName,
     lastName,
     age,
     position,
-    team,
+    team: teamExists.id,
   });
 
+  await Team.findByIdAndUpdate(teamExists.id, { $push: { players: profile } });
+
   res.status(201).json(profile);
+});
+
+app.post('/teams', async (req, res) => {
+  const { name, coach } = req.body;
+
+  const teamExists = await Team.findOne({ name });
+
+  if (teamExists) {
+    return res.status(400).json({ message: 'Team already exists' });
+  }
+
+  const team = await Team.create({ name, coach });
+
+  res.status(201).json(team);
 });
 
 export { app };
