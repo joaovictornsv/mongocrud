613d636ca8170840642f75cc7f48643ee5dd944d
Joao Victor Negreiros <joaovictornsv@gmail.com>
2021-06-05 23:42:38 -0300

    feat: create and use PlayerController

---
+++ src/controllers/PlayerController.ts
@@ -0,0 +1,49 @@
+import { Request, Response } from 'express';
+import Player from '../entities/Player';
+import Team from '../entities/Team';
+
+class PlayerController {
+  async indexAll(req: Request, res: Response) {
+    const profiles = await Player.find().populate({ path: 'team', select: 'name coach -_id' });
+
+    res.status(200).json(profiles);
+  }
+
+  async create(req: Request, res: Response) {
+    const {
+      firstName,
+      lastName,
+      age,
+      position,
+      team,
+    } = req.body;
+
+    if (!firstName || !lastName || !age || !position) {
+      return res.status(400).json({ message: 'Provide the required fields' });
+    }
+
+    if (!team) {
+      return res.status(400).json({ message: 'Team not provided' });
+    }
+
+    const teamExists = await Team.findOne({ name: team });
+
+    if (!teamExists) {
+      return res.status(400).json({ message: 'Team provided not found' });
+    }
+
+    const profile = await Player.create({
+      firstName,
+      lastName,
+      age,
+      position,
+      team: teamExists.id,
+    });
+
+    await Team.findByIdAndUpdate(teamExists.id, { $push: { players: profile } });
+
+    res.status(201).json(profile);
+  }
+}
+
+export { PlayerController };
--- src/routers/playerRouter.ts
+++ src/routers/playerRouter.ts
@@ -1,49 +1,12 @@
 import { Router } from 'express';
-import Player from '../entities/Player';
-import Team from '../entities/Team';
+import { PlayerController } from '../controllers/PlayerController';
 
 const router = Router();
 
-router.get('/', async (req, res) => {
-  const profiles = await Player.find().populate({ path: 'team', select: 'name coach -_id' });
+const playerController = new PlayerController();
 
-  res.status(200).json(profiles);
-});
+router.get('/', playerController.indexAll);
 
-router.post('/', async (req, res) => {
-  const {
-    firstName,
-    lastName,
-    age,
-    position,
-    team,
-  } = req.body;
-
-  if (!firstName || !lastName || !age || !position) {
-    return res.status(400).json({ message: 'Provide the required fields' });
-  }
-
-  if (!team) {
-    return res.status(400).json({ message: 'Team not provided' });
-  }
-
-  const teamExists = await Team.findOne({ name: team });
-
-  if (!teamExists) {
-    return res.status(400).json({ message: 'Team provided not found' });
-  }
-
-  const profile = await Player.create({
-    firstName,
-    lastName,
-    age,
-    position,
-    team: teamExists.id,
-  });
-
-  await Team.findByIdAndUpdate(teamExists.id, { $push: { players: profile } });
-
-  res.status(201).json(profile);
-});
+router.post('/', playerController.create);
 
 export { router as playerRouter };
