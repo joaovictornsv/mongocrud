7e0f6fe43d95d5184c69759774d704ef268ebe55
Joao Victor Negreiros <joaovictornsv@gmail.com>
2021-06-07 13:35:02 -0300

    test: create mocks folder

---
+++ __mocks__/entities/Player.mock.ts
@@ -0,0 +1,21 @@
+export const PlayerResquestMock = {
+  firstName: 'User Example',
+  lastName: 'Mock',
+  age: 20,
+  position: 'mock-position',
+  team: 'mock-team',
+};
+
+export const PlayerResponseMock = {
+  _id: 'mock-id',
+  firstName: 'User Example',
+  lastName: 'Mock',
+  age: 20,
+  team: 'mock-team-id',
+  position: 'mock-position',
+  created_at: 'mock-created-at',
+  updated_at: 'mock-updated-at',
+  __v: 0,
+};
+
+export const PlayerArrayResponseMock = [];
---
+++ __mocks__/entities/Team.mock.ts
@@ -0,0 +1,12 @@
+export const TeamResquestMock = {
+  name: 'team-mock',
+  coach: 'coach-mock',
+};
+
+export const TeamResponseMock = {
+  _id: 'mock-id',
+  name: 'team-mock',
+  players: [],
+  coach: 'coach-mock',
+  __v: 0,
+};
