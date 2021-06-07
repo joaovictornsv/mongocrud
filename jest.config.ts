79e581f3fa53ef9335a8ecb913789ec583ba4ffb
Joao Victor Negreiros <joaovictornsv@gmail.com>
2021-06-07 13:34:09 -0300

    fix: collect coverage

--- jest.config.ts
+++ jest.config.ts
@@ -34,10 +34,6 @@ export default {
   // An array of glob patterns indicating a set of files for which coverage information should be collected
   collectCoverageFrom: [
     'src/controllers/*.ts',
-    'src/repositories/*.ts',
-    'src/services/*.ts',
-    'src/middlewares/*.ts',
-    'src/validators/*.ts',
   ],
 
   // The directory where Jest should output its coverage files
